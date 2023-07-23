2d70393a1d61e0d3fd4b7ca69f04c90ab6a87da4
Jakob Hellermann <jakob.hellermann@protonmail.com>
2023-07-23 20:32:21 +0200

    Initial Commit

---
+++ DebugMod/DebugMod.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using JetBrains.Annotations;
+using ModdingAPI;
+
+namespace MoonscarsDebugMod.DebugMod;
+
+[UsedImplicitly]
+internal class DebugMod : Mod {
+    public override string GetName() => Assembly.GetExecutingAssembly().GetName().Name;
+
+    public override string Version() => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+
+    public override void Load() {
+        Logger.Log("Loaded DebugMod");
+    }
+
+    public override void Unload() {
+        Logger.Log("Unloaded DebugMod");
+    }
+}
